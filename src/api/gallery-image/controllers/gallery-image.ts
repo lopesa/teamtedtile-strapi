3400cb631098d0bd0d07c42ce6368aa2c69700f3
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2022-12-14 18:22:17 -0400

    add next/prev to the GalleryItem return obj

--- src/api/gallery-image/controllers/gallery-image.ts
+++ src/api/gallery-image/controllers/gallery-image.ts
@@ -2,15 +2,24 @@
  * gallery-image controller
  */
 
-import { factories } from "@strapi/strapi";
+import { factories, MediaAttribute } from "@strapi/strapi";
 import { ApiGalleryImageGalleryImage } from "../../../../schemas";
+
+interface GalleryImageApiReplyObject {
+  title: string;
+  image: MediaAttribute;
+  copyright?: string;
+  tedheadText?: string;
+  previous?: string;
+  next?: string;
+}
 
 /**
  *
  * @param entries
  * @returns ordered entries
  *
- * incorpoirates the forcedOrder setting
+ * incorporates the forcedOrder setting
  * ordering is 1st forcedOrder, then ID order
  */
 const orderEntries = (
@@ -36,28 +45,34 @@ const orderEntries = (
   return finalEntries;
 };
 
+/**
+ * create an api return object from each GalleryItem.
+ * Only return select fields (strip out many fields),
+ * and add the next and previous fields
+ * @param entries
+ */
+const parseEntriesForReturn = (
+  entries: ApiGalleryImageGalleryImage["attributes"][]
+): GalleryImageApiReplyObject[] => {
+  let returnArr = [];
+  entries.forEach((entry, index) => {
+    const newObj = {};
+    newObj["title"] = entry.title;
+    newObj["image"] = entry.image;
+    newObj["copyright"] = entry.copyright;
+    newObj["tedheadText"] = entry.tedheadText;
+    newObj["previous"] = entries[index - 1]?.title;
+    newObj["next"] = entries[index + 1]?.title;
+    returnArr.push(newObj);
+  });
+  return returnArr;
+};
+
 export default factories.createCoreController(
   "api::gallery-image.gallery-image",
   ({ strapi }) => ({
     async find(ctx) {
-      // if a title is specified, return early with
-      // only the entry with that title (gallery pages)
-      if (ctx.query.filters?.title) {
-        const entry: ApiGalleryImageGalleryImage["attributes"] =
-          await strapi.entityService.findMany(
-            "api::gallery-image.gallery-image",
-            {
-              filters: {
-                title: ctx.query.filters.title.$eq,
-              },
-              populate: "*",
-            }
-          );
-        return this.transformResponse(entry);
-      }
-
-      // Standard Case - find all entries and order by forceOrder then ID
-      // 1
+      // 1 - find all entries and order by forceOrder then ID
       const entries: ApiGalleryImageGalleryImage["attributes"][] =
         await strapi.entityService.findMany(
           "api::gallery-image.gallery-image",
@@ -68,10 +83,27 @@ export default factories.createCoreController(
         );
 
       // @TODO orderEntries can probably be replaced with dql or strapi multiple field ordering
-      let orderedEntries = orderEntries(entries);
+      const orderedEntries = orderEntries(entries);
+
+      // parse entries for return (only send select props. add next/previous)
+      let entriesParsedForReturn = parseEntriesForReturn(orderedEntries);
+
+      // if a title filter is passed, only return that entry
+      // because I want to fetch individual entries by title
+      // api is based on ID
+      if (ctx.query.filters?.title) {
+        entriesParsedForReturn = [
+          entriesParsedForReturn.find(
+            (entry) => entry.title === ctx.query.filters.title.$eq
+          ),
+        ];
+      }
 
       // 2
-      const sanitizedEntries = await this.sanitizeOutput(orderedEntries, ctx);
+      const sanitizedEntries = await this.sanitizeOutput(
+        entriesParsedForReturn,
+        ctx
+      );
 
       // 3
       return this.transformResponse(sanitizedEntries);
@@ -80,6 +112,7 @@ export default factories.createCoreController(
     // example of a custom controller method
     // taken from here: https://strapi.io/blog/strapi-internals-customizing-the-backend-part-1-models-controllers-and-routes
     // see corresponding route in src/api/gallery-image/routes/gallery-image-test.ts
+    // (cannot comment this out without removing that file or build will fail)
     async test(ctx) {
       ctx.body = "test";
     },
