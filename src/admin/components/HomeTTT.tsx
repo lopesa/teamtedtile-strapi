eb73632760b9c609de2117a25949b796938a4c50
Tony Lopes <tonylopes@tonys-mac-mini.lan>
2023-01-25 15:31:41 -0400

    add the current build time and link info. Some copy tweaks

--- src/admin/components/HomeTTT.tsx
+++ src/admin/components/HomeTTT.tsx
@@ -9,8 +9,12 @@ function HomeSplash() {
   >(false);
   const [error, setError] = useState<string | null>(null);
   const [buildEnvIsReady, setBuildEnvIsReady] = useState<boolean>(false);
+  const [currentProdDeployURL, setCurrentProdDeployURL] = useState<string>("");
+  const [currentProdDeployTime, setCurrentProdDeployTime] =
+    useState<string>("");
   const CHECK_BUILD_ENVIRONMENT_READY_ENDPOINT =
     "/build/get-build-environment-is-ready";
+  const GET_CURRENT_PROD_DEPLOY_ENDPOINT = "/build/get-current-prod-deploy";
   const DEPLOY_ENDPOINT = "/build";
 
   const getApiUrlBase = useCallback(() => {
@@ -32,6 +36,16 @@ function HomeSplash() {
       return await res.json();
     }
   }, [getApiUrlBase, CHECK_BUILD_ENVIRONMENT_READY_ENDPOINT]);
+
+  const getCurrentProdDeploy = useCallback(async () => {
+    const res = await fetch(
+      `${getApiUrlBase()}${GET_CURRENT_PROD_DEPLOY_ENDPOINT}`
+    ).catch((e) => {});
+    if (res && res.ok) {
+      return await res.json();
+    }
+    return null;
+  }, [getApiUrlBase, GET_CURRENT_PROD_DEPLOY_ENDPOINT]);
 
   const doErrorProcesses = (errorMessage: string) => {
     setError(errorMessage);
@@ -68,20 +82,25 @@ function HomeSplash() {
   };
 
   useEffect(() => {
-    getBuildEnvIsReady()
-      .then((isReady) => {
-        setBuildEnvIsReady(isReady);
+    getCurrentProdDeploy()
+      .then((currentProdDeploy) => {
+        setBuildEnvIsReady(Boolean(currentProdDeploy));
+        setCurrentProdDeployURL(currentProdDeploy.alias[0]);
+        setCurrentProdDeployTime(
+          new Date(currentProdDeploy.createdAt).toLocaleString()
+        );
       })
       .catch((e) => {
         setBuildEnvIsReady(null);
         setError(e.message);
       });
-  }, [getBuildEnvIsReady]);
+  }, [getCurrentProdDeploy]);
 
   const containerStyle = {
-    width: "80%",
+    width: "90%",
     maxWidth: "600px",
-    margin: "75px 0 0 75px",
+    padding: "10px",
+    margin: "50px 0 50px 30px",
     color: "white",
   };
 
@@ -124,14 +143,19 @@ function HomeSplash() {
     fontWeight: "bold",
   };
 
+  // const aStyle = {
+  //   textDecoration: "underline",
+  //   color: "blue",
+  //   pointer: "cursor",
+  // }
+
   return (
     <div style={containerStyle}>
-      <h2 style={h2Style}>
-        Welcome to the Team Ted Tile Content Management System and Build Panel!
-      </h2>
+      <h2 style={h2Style}>Welcome!</h2>
       <p style={pStyle}>
-        Here, you can change much of the site content, and you can "build" the
-        site to make those changes live.
+        This is the Team Ted Tile{"\n"}Content Management System and Build
+        Panel. Here, you can change much of the site content, and you can
+        "build" the site to make those changes live.
       </p>
       <p style={pStyle}>
         You can change items by clicking the "Content Manager" link in the
@@ -139,22 +163,18 @@ function HomeSplash() {
         different content controls. The items you can change include:
         <ul style={ulStyle}>
           <li style={liStyle}>
-            <b style={bStyle}>• Gallery Content</b>: Change Gallery items from
-            the Gallery Image Collection Type link in the left sub-sidebar. The
-            images are required to have a Title and an Image and note: THE IMAGE
-            TITLE WILL BECOME ITS URL. It's best if it's something human
-            readable for the search engines. Also note: the image that you
-            upload does not have to have any particular file name. (In the past
-            version of this site, the image's name was used as its url.) Other
-            than the image you can add a Copyright and "tedHeadText" (lol) which
-            adds the little popover caption that has Ted's head as the icon
+            • Gallery Content: Change Gallery items from the Gallery Image
+            Collection Type link in the left sub-sidebar. The images are
+            required to have a Title and an Image and note: THE IMAGE TITLE WILL
+            BECOME ITS URL. It's best if it's something human readable for the
+            search engines. Also note: the image that you upload does not have
+            to have any particular file name. (In the past version of this site,
+            the image's name was used as its url.) Other than the image you can
+            add a Copyright and "tedHeadText" (lol) which adds the little
+            popover caption that has Ted's head as the icon
           </li>
-          <li style={liStyle}>
-            <b style={bStyle}>• About Page content</b>
-          </li>
-          <li style={liStyle}>
-            <b style={bStyle}>• Contact Page copy</b>
-          </li>
+          <li style={liStyle}>• About Page content</li>
+          <li style={liStyle}>• Contact Page copy</li>
         </ul>
       </p>
       <p style={pStyle}>
@@ -170,6 +190,21 @@ function HomeSplash() {
         live url
       </p>
       <hr style={{ marginTop: "35px" }}></hr>
+      <h2 style={h2Style}>Current Deployments</h2>
+      {currentProdDeployURL && (
+        <p style={pStyle}>
+          <a
+            href={`https://${currentProdDeployURL}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {currentProdDeployURL}
+          </a>
+        </p>
+      )}
+      {currentProdDeployTime && (
+        <p style={pStyle}>Built at: {currentProdDeployTime}</p>
+      )}
       <h2 style={h2Style}>Deployment Actions</h2>
 
       {buildEnvIsReady && (
@@ -211,7 +246,43 @@ function HomeSplash() {
       )}
 
       {error && <p>Error: {error}</p>}
-      <h4 style={h4Style}>TODO: revert to most recent production version</h4>
+      <hr style={{ marginTop: "35px" }}></hr>
+      <h4 style={h4Style}>Ted!! I will remove this section eventually...</h4>
+      <p style={pStyle}>
+        For now, we can use it to keep track of TODO items. I'll add all the
+        things I'm thinking about doing/fixing before we go live with this
+        version of things. This is for both this cms as well as for the site
+        itself (I rebuilt that as well).
+      </p>
+      <p style={pStyle}>
+        Please, as you have the chance to review any of this, again both this
+        CMS as well as the site, make notes of things you see are broken
+        (nothing too small even if you think I probably already know about it)
+        or also make notes of things you may want to change. Now's the time, I
+        may not get a chance to work on this for another 5 years lol. You can
+        send me an email and I can port them into this place or just do them.
+      </p>
+      <p style={pStyle}>Ok, so here's things I know I need to do</p>
+      <ul style={ulStyle}>
+        <li style={liStyle}>
+          • CMS: make a "revert to most recent working production version"
+          button
+        </li>
+        <li style={liStyle}>• SITE: make slideshow swipeable on devices</li>
+        <li style={liStyle}>• SITE: contact form validation</li>
+        <li style={liStyle}>• SITE: google analytics events</li>
+      </ul>
+      <h4 style={h4Style}>A question for you Ted:</h4>
+      <ul style={ulStyle}>
+        <li style={liStyle}>
+          • How are you feeling about the Facebook and Twitter links out? I can
+          check the data (maybe) about usage, but have you ever seen any of your
+          work posted over to those sites, especially via your website? If it's
+          not happening it may be cleaner to remove them. If you want it to
+          happen we could probably push it somehow. I haven't currently done the
+          implementation work in the new site instance.
+        </li>
+      </ul>
     </div>
   );
 }
